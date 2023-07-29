19b9166b2b94daff5d488605ecb69a4ffe46d7ad
randyg111 <ranboygt@gmail.com>
2023-07-29 18:02:36 -0500

    Create addresses file

--- src/main/java/EmailManager.java
+++ src/main/java/EmailManager.java
@@ -16,16 +16,22 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 /* class to demonstrate use of Gmail list labels API */
 public class EmailManager {
   /**
    * Application name.
    */
-  private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+  private static final String APPLICATION_NAME = "Email Manager";
   /**
    * Global instance of the JSON factory.
    */
@@ -41,6 +47,16 @@ public class EmailManager {
    */
   private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
   private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+  /**
+   * Directory to store email addresses whose emails should be deleted.
+   */
+  private static final String ADDRESSES_FILE_PATH = "addresses.txt";
+
+  /**
+   * Length of time to wait before email deletion.
+   */
+  private static final int EMAIL_DELETION_TIME = 7;
 
   /**
    * Creates an authorized Credential object.
@@ -71,6 +87,8 @@ public class EmailManager {
     return credential;
   }
 
+  // Prompt for each address and deletion interval
+  // Prompt to unsubscribe from unread emails
   public static void main(String... args) throws IOException, GeneralSecurityException {
     // Build a new authorized API client service.
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -90,5 +108,27 @@ public class EmailManager {
         System.out.printf("- %s\n", label.getName());
       }
     }
+
+    // Create file with email addresses whose emails should be deleted
+    Path addressesFile = Paths.get(EmailManager.class.getResource("/").getPath() + ADDRESSES_FILE_PATH);
+    if (Files.notExists(addressesFile)) {
+      Files.createFile(addressesFile);
+    }
+    Set<String> addresses = getAddresses(addressesFile);
+
+    // Write to addresses file if necessary
+//    PrintWriter writer = new PrintWriter(Files.newBufferedWriter(addressesFile));
+//
+//    writer.close();
+  }
+
+  private static Set<String> getAddresses(Path addressesFile) throws IOException {
+    BufferedReader reader = Files.newBufferedReader(addressesFile);
+    Set<String> addresses = new HashSet<>();
+    String line = reader.readLine();
+    while ((line = reader.readLine()) != null) {
+      addresses.add(line);
+    }
+    return addresses;
   }
 }
